1525fb2cfdb876da7124cf3a8c1b48140a73ae35
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-19 11:31:51 +0500

    добавлены админки курсов, модулей и уроков

--- courses/admin.py
+++ courses/admin.py
@@ -6,3 +6,18 @@ from .models import Category, Course, Module, Lesson
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'icon', 'created_at', 'updated_at')
     search_fields = ('name', 'description', 'icon')
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'teacher', 'category', 'price', 'discount_price', 'is_published', 'created_at', 'updated_at')
+    search_fields = ('title', 'description', 'category', 'price', 'discount_price')
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course', 'title', 'order', 'created_at', 'updated_at')
+    search_fields = ('title', 'description', 'course', 'order')
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'module', 'title', 'video_url', 'duration', 'order')
+    search_fields = ('module', 'title', 'content', 'video_url', 'order')

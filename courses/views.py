8b7d038d618846d58c71a2fb41c9d71a131233de
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-23 20:28:24 +0500

    categories, courses and modules permissions created

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from .models import Category, Course, Module, Lesson
 from .serializers import CategorySerializer, CourseSerializer, ModulesSerializer, LessonSerializer
 from .pagination import CategoryPagination, CoursePagination, ModulePagination, LessonPagination
 from rest_framework import generics
+from users.permissions import IsTeacher, IsCourseTeacherOrAdmin
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -10,10 +12,22 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     pagination_class = CategoryPagination
 
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAdminUser()]
+
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     pagination_class = CoursePagination
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsTeacher()]
+        elif self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [IsCourseTeacherOrAdmin()]
+        return [AllowAny()]
 
 class CoursesByCategory(generics.ListAPIView):
     serializer_class = CourseSerializer
@@ -28,9 +42,15 @@ class ModuleViewSet(viewsets.ModelViewSet):
     serializer_class = ModulesSerializer
     pagination_class = ModulePagination
 
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticated()]
+        return [IsCourseTeacherOrAdmin()]
+
 class ModulesByCourse(generics.ListAPIView):
     serializer_class = ModulesSerializer
     pagination_class = ModulePagination
+    permission_classes = [IsAuthenticated()]
 
     def get_queryset(self):
         course_id = self.kwargs['course_id']
--- users/permissions.py
+++ users/permissions.py
@@ -6,3 +6,11 @@ class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return request.user.is_authenticated
         return obj == request.user or request.user.is_staff
+
+class IsTeacher(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and request.user.is_teacher)
+
+class IsCourseTeacherOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff or (obj.teacher == request.user)

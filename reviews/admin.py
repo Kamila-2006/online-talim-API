27ab647d1f63fc9f3eb0b54d08ffeedefdee6ced
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-24 16:02:35 +0500

    reviews admin  created

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'course', 'rating')
+    search_fields = ('user', 'course', 'rating')

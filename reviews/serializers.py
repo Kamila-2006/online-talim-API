d74ad4def6ce887bb8ed9a59191ae9e70011db4b
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-04-25 12:36:14 +0500

    reviews created

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
 from .models import Review
+from courses.models import Course
+from users.serializers import UserShortSerializer
+from courses.serializers import CourseShortSerializer
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    user = UserShortSerializer(read_only=True)
+    course = serializers.PrimaryKeyRelatedField(queryset=Course.objects.all())
+
     class Meta:
         model = Review
-        fields = ['id', 'user', 'course', 'rating', 'comment']
-        read_only_fields = ['id',]
+        fields = ['id', 'user', 'course', 'rating', 'comment', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Review.objects.create(user=user, **validated_data)
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['course'] = CourseShortSerializer(instance.course).data
+        return rep
